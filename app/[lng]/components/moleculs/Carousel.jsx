e504ae0f3a2535a452e1b2afcc9554b10942a6e3
trupletj <tuvshinjargal01@gmail.com>
2023-04-03 19:09:30 +0800

    sl

--- app/[lng]/components/moleculs/Carousel.jsx
+++ app/[lng]/components/moleculs/Carousel.jsx
@@ -1,8 +1,32 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/swiper.min.css";
 
 function Carousel() {
-  return <div>Carousel</div>;
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+
+  if (!domLoaded) {
+    null;
+  }
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  );
 }
 
 export default Carousel;
