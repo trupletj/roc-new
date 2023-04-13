5fd2aede5c2a0ce6ae7fe6a99e272fd301f8de29
trupletj <tuvshinjargal01@gmail.com>
2023-04-13 21:36:11 +0800

    ss

--- app/[lng]/components/atoms/Divider.jsx
+++ app/[lng]/components/atoms/Divider.jsx
@@ -4,7 +4,7 @@ function Divider({ my }) {
   const space = my || "100px";
   return (
     <div
-      className={`my-[${space}] w-full max-w-[1640px] mx-auto h-[1px] border-t-0 bg-black  dark:bg-white opacity-100 `}
+      className={`my-[100px] w-full max-w-[1640px] mx-auto h-[1px] border-t-0 bg-black  dark:bg-white opacity-100 `}
     />
   );
 }
