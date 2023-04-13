656d7b05828137faa8d51a8a86b643c658df5c5e
Enkhjinshde <enkhjin1992@gmail.com>
2023-04-13 18:17:58 +0800

    shop list

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // reactStrictMode: true,
   experimental: {
     appDir: true,
   },
   images: {
     domains: [
       "localhost",
+      "api.app-roc.com",
       "res.cloudinary.com",
       "picsum.photos",
       "cdn.shopify.com",
