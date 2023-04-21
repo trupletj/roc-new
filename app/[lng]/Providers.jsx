80ba8073f69441a8336e474ad394094952982813
Enkhjinshde <enkhjin1992@gmail.com>
2023-04-21 08:17:36 +0800

    aa

--- app/[lng]/Providers.jsx
+++ app/[lng]/Providers.jsx
@@ -6,9 +6,9 @@ import GlobalContext from "./context/GlobalContext";
 function Providers({ children }) {
   const [openBasket, setOpenBasket] = useState(false);
   const [openLogin, setOpenLogin] = useState(false);
-  // const api_domain = "http://192.168.10.214:8000";
+  const api_domain = "http://192.168.10.214:8000";
 
-  const api_domain = "https://api.app-roc.com";
+  // const api_domain = "https://api.app-roc.com";
 
   useEffect(() => {
     console.log("mounted");
