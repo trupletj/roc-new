73d83f845a01440a92d0fadcde6da8738d1531a5
Enkhjinshde <enkhjin1992@gmail.com>
2023-05-14 20:43:25 +0800

    ee

--- app/[lng]/components/Checkout/UserOrderInformation.jsx
+++ app/[lng]/components/Checkout/UserOrderInformation.jsx
@@ -38,7 +38,7 @@ function UserOrderInformation({ lng }) {
       addressType: null,
     },
   });
-  console.log(errors);
+
   const [addresses, setAddresses] = useState(
     user?.addresses
       ? [
@@ -66,22 +66,6 @@ function UserOrderInformation({ lng }) {
         ]
   );
   const [currentAddress, setCurrentAddress] = useState(false);
-  const {
-    register: address_register,
-    handleSubmit: handdleAddressSubmit,
-    formState: { errors: address_error },
-    reset: registerReset,
-  } = useForm({
-    defaultValues: {
-      name: currentAddress?.name || null,
-      receiver_name: currentAddress?.receiver_name || null,
-      receiver_phone: currentAddress?.receiver_phone || null,
-      district: currentAddress?.district || null,
-      address_information: currentAddress?.address_information || null,
-      lat_lng: currentAddress?.lat_lng || null,
-    },
-  });
-
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -108,95 +92,6 @@ function UserOrderInformation({ lng }) {
   }, [isLoaded]);
   //End form confirm nemev
   const onSubmit = (data) => console.log(data);
-  const onSubmitAddress = async (data) => {
-    if (showMarker) {
-      data.lat_lng = JSON.stringify(center);
-    }
-    if (currentAddress) {
-      data.addressType = currentAddress.id;
-    }
-    setIsSaving(true);
-    const response = await fetcher(
-      "client/profile/address/create",
-      data,
-      token
-    );
-    if (response.status == 200) {
-      const result = await response.json();
-      setUser(result.user);
-      setCurrentAddress(result.address);
-      setAddresses(
-        result.user?.addresses
-          ? [
-              ...result.user.addresses,
-              {
-                id: 0,
-                name: "",
-                receiver_name: "",
-                receiver_phone: "",
-                district: "",
-                address_information: "",
-                lat_lng: "",
-              },
-            ]
-          : [
-              {
-                id: 0,
-                name: "",
-                receiver_name: "",
-                receiver_phone: "",
-                district: "",
-                address_information: "",
-                lat_lng: "",
-              },
-            ]
-      );
-      setIsSaving(false);
-    }
-  };
-  const deleteAddress = async () => {
-    if (currentAddress) {
-      if (confirm(t("delete_address"))) {
-        setIsSaving(true);
-        const response = await fetcher(
-          "client/profile/address/delete",
-          currentAddress,
-          token
-        );
-        if (response.status == 200) {
-          const result = await response.json();
-          setUser(result.user);
-          setAddresses(
-            result.user?.addresses
-              ? [
-                  ...result.user.addresses,
-                  {
-                    id: 0,
-                    name: "",
-                    receiver_name: "",
-                    receiver_phone: "",
-                    district: "",
-                    address_information: "",
-                    lat_lng: "",
-                  },
-                ]
-              : [
-                  {
-                    id: 0,
-                    name: "",
-                    receiver_name: "",
-                    receiver_phone: "",
-                    district: "",
-                    address_information: "",
-                    lat_lng: "",
-                  },
-                ]
-          );
-          setIsSaving(false);
-        }
-      }
-    }
-  };
   const handleDragEnd = (e) => {
     setZoom(14);
     setCenter({ lat: e.latLng.lat(), lng: e.latLng.lng() });
@@ -219,10 +114,11 @@ function UserOrderInformation({ lng }) {
             </label>
             <div className="mt-2">
               <input
+                disabled
                 placeholder={t("first_name")}
                 type="text"
                 {...register("first_name", {
-                  required: true,
+                  required: false,
                   maxLength: 20,
                 })}
                 name="first_name"
@@ -246,10 +142,11 @@ function UserOrderInformation({ lng }) {
             </label>
             <div className="mt-2">
               <input
+                disabled
                 placeholder={t("last_name")}
                 type="text"
                 {...register("last_name", {
-                  required: true,
+                  required: false,
                   maxLength: 20,
                 })}
                 name="last_name"
@@ -276,7 +173,7 @@ function UserOrderInformation({ lng }) {
               <input
                 disabled={true}
                 placeholder={t("phone")}
-                {...register("phone", { required: true, maxLength: 8 })}
+                {...register("phone", { required: false, maxLength: 8 })}
                 type="text"
                 name="phone"
                 id="phone"
@@ -299,10 +196,11 @@ function UserOrderInformation({ lng }) {
             </label>
             <div className="mt-2">
               <input
+                disabled
                 placeholder={t("email")}
                 type="text"
                 {...register("email", {
-                  required: true,
+                  required: false,
                   pattern: {
                     value: /\S+@\S+\.\S+/,
                     message: "Entered value does not match email format",
@@ -464,15 +362,41 @@ function UserOrderInformation({ lng }) {
                           required: true,
                         })}
                         type="radio"
+                        name="addressType"
                         id={`address-${address.id}`}
-                        value={address.id}
                         className="peer hidden"
+                        onChange={(e) => {
+                          if (e.target.checked) {
+                            setCurrentAddress(address);
+                            if (address?.lat_lng) {
+                              try {
+                                let l_lng = JSON.parse(address.lat_lng);
+                                if (l_lng.lat && l_lng.lng) {
+                                  setCenter(l_lng);
+                                  setMain(l_lng);
+                                  setShowMarker(true);
+                                } else {
+                                  setShowMarker(false);
+                                }
+                              } catch (e) {
+                                setShowMarker(false);
+                              }
+                            } else {
+                              setShowMarker(false);
+                            }
+                          }
+                          return e.target.checked;
+                        }}
                       />
                       <label
                         htmlFor={`address-${address.id}`}
-                        className={`block cursor-pointer select-none   p-2 text-center   text-white  peer-checked:bg-[#F0B450] bg-[#080505]`}
+                        className={`block cursor-pointer select-none   p-2 text-center   text-white ${
+                          currentAddress?.id == address.id
+                            ? "bg-[#F0B450]"
+                            : "bg-[#080505]"
+                        }`}
                       >
-                        {address.name}
+                        {address.id === 0 ? t("new") : address.name}
                       </label>
                     </div>
                   );
@@ -485,122 +409,8 @@ function UserOrderInformation({ lng }) {
               )}
             </div>
           </div>
-          <div className="sm:col-span-6">
-            <button
-              htmlFor={2}
-              type="submit"
-              className="block cursor-pointer select-none  bg-[#080505] p-2 text-center   text-white w-full"
-            >
-              {t("save")}
-            </button>
-            {isSaving && <Loading />}
-          </div>
-        </div>
-      </form>
-      <form
-        className="w-full pt-10"
-        onSubmit={handdleAddressSubmit(onSubmitAddress)}
-      >
-        <h2 className="text-2xl  text-gray-900 uppercase font-normal">
-          {t("delivery_address")}
-        </h2>
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 md:grid-cols-6 bg-white p-10">
-          <div className="md:col-span-6">
-            <label
-              htmlFor="addressType"
-              className="block text-base font-light leading-6 text-gray-900"
-            >
-              <span className="text-red-500 mr-2">*</span>
-              {t("choose_address")}
-            </label>
-            <div className="grid  grid-cols-3 gap-2 mt-2">
-              {addresses?.map((address) => {
-                return (
-                  <div
-                    className="col-span-3 md:col-span-1"
-                    key={`address-item-${address.id}`}
-                  >
-                    <input
-                      placeholder={t("choose_address")}
-                      {...address_register("addressType", { required: false })}
-                      type="radio"
-                      name="option"
-                      id={`address-${address.id}`}
-                      className="peer hidden"
-                      onChange={(e) => {
-                        if (e.target.checked) {
-                          setCurrentAddress(address);
-                          registerReset({
-                            addressType: address?.id || 0,
-                            name: address?.name || null,
-                            receiver_name: address?.receiver_name || null,
-                            receiver_phone: address?.receiver_phone || null,
-                            district: address?.district || null,
-                            address_information:
-                              address?.address_information || null,
-                            lat_lng: address?.lat_lng || null,
-                          });
-
-                          if (address?.lat_lng) {
-                            console.log(address?.lat_lng);
-
-                            setCenter(JSON.parse(address.lat_lng));
-                            setMain(JSON.parse(address.lat_lng));
-                            setShowMarker(true);
-                          } else {
-                            setShowMarker(false);
-                          }
-                        }
-                        return e.target.checked;
-                      }}
-                    />
-                    <label
-                      htmlFor={`address-${address.id}`}
-                      className={`block cursor-pointer select-none   p-2 text-center   text-white ${
-                        currentAddress?.id == address.id
-                          ? "bg-[#F0B450]"
-                          : "bg-[#080505]"
-                      }`}
-                    >
-                      {address.id === 0 ? t("new") : address.name}
-                    </label>
-                  </div>
-                );
-              })}
-
-              {address_error.addressType && (
-                <div className="mt-4 col-span-3 bg-red-500 py-2 px-5">
-                  <h1 className="text-white text-sm">{t("please_select")}</h1>
-                </div>
-              )}
-            </div>
-          </div>
           {currentAddress && (
             <>
-              <div className="sm:col-span-6">
-                <label
-                  htmlFor="first-name"
-                  className="block text-base font-light leading-6 text-gray-900"
-                >
-                  <span className="text-red-500 mr-2">*</span>
-                  {t("address_name")}
-                </label>
-                <div className="mt-2">
-                  <input
-                    placeholder={t("address_name")}
-                    {...address_register("name", { required: true })}
-                    type="text"
-                    name="name"
-                    id="address_name"
-                    className="block w-full px-3  border border-[#707070]  py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm bg-white"
-                  />
-                </div>
-                {address_error.name && (
-                  <div className="mt-4 w-full bg-red-500 py-2 px-5">
-                    <h1 className="text-white text-sm">{t("please_fill")}</h1>
-                  </div>
-                )}
-              </div>
               <div className="sm:col-span-3">
                 <label
                   htmlFor="first-name"
@@ -611,19 +421,15 @@ function UserOrderInformation({ lng }) {
                 </label>
                 <div className="mt-2">
                   <input
+                    disabled
                     placeholder={t("receiver_name")}
-                    {...address_register("receiver_name", { required: true })}
+                    value={currentAddress.receiver_name}
                     type="text"
                     name="receiver_name"
                     id="receiver_name"
-                    className="block w-full px-3  border border-[#707070]  py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm bg-white"
+                    className="block w-full px-3  border border-[#707070]  py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm bg-gray-400"
                   />
                 </div>
-                {address_error.receiver_name && (
-                  <div className="mt-4 w-full bg-red-500 py-2 px-5">
-                    <h1 className="text-white text-sm">{t("please_fill")}</h1>
-                  </div>
-                )}
               </div>
               <div className="sm:col-span-3">
                 <label
@@ -635,19 +441,15 @@ function UserOrderInformation({ lng }) {
                 </label>
                 <div className="mt-2">
                   <input
+                    disabled
                     placeholder={t("receiver_phone")}
-                    {...address_register("receiver_phone", { required: true })}
+                    value={currentAddress.receiver_phone}
                     type="text"
                     name="receiver_phone"
                     id="receiver_phone"
-                    className="block w-full  px-3 border border-[#707070]  py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm bg-white"
+                    className="block w-full  px-3 border border-[#707070]  py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm bg-gray-400"
                   />
                 </div>
-                {address_error.receiver_phone && (
-                  <div className="mt-4 w-full bg-red-500 py-2 px-5">
-                    <h1 className="text-white text-sm">{t("please_fill")}</h1>
-                  </div>
-                )}
               </div>
               <div className="sm:col-span-6">
                 <label
@@ -659,10 +461,10 @@ function UserOrderInformation({ lng }) {
                 </label>
                 <div className="mt-2">
                   <select
-                    defaultValue={"BZD"}
-                    {...address_register("district", { required: true })}
+                    disabled
+                    value={currentAddress.district}
                     type="select"
-                    className="block w-full  border border-[#707070] px-2  py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm bg-white"
+                    className="block w-full  border border-[#707070] px-2  py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm bg-gray-400"
                   >
                     <option value={"BGD"}>Баянгол</option>
                     <option value={"BZD"}>Баянзүрх</option>
@@ -671,11 +473,6 @@ function UserOrderInformation({ lng }) {
                     <option value={"CHD"}>Чингэлтэй</option>
                     <option value={"HUD"}>Хан-Уул</option>
                   </select>
-                  {address_error.district && (
-                    <div className="mt-4 w-full bg-red-500 py-2 px-5">
-                      <h1 className="text-white text-sm">Заавал бөглөх</h1>
-                    </div>
-                  )}
                 </div>
               </div>
 
@@ -688,13 +485,14 @@ function UserOrderInformation({ lng }) {
                 </label>
                 <div className="mt-2">
                   <textarea
-                    {...address_register("address_information")}
+                    disabled
+                    value={currentAddress.address_information}
                     rows={4}
                     type="text"
                     name="address_information"
                     id="address_information"
                     placeholder={t("full_address")}
-                    className="block w-full  border border-[#707070] px-2  py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm bg-white"
+                    className="block w-full  border border-[#707070] px-2  py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm bg-gray-400"
                   />
                 </div>
               </div>
@@ -740,32 +538,11 @@ function UserOrderInformation({ lng }) {
                   )}
                 </div>
               </div>
-              <div className="sm:col-span-6">
-                <button
-                  htmlFor={2}
-                  type="submit"
-                  className="block cursor-pointer select-none  bg-[#080505] p-2 text-center   text-white w-full"
-                >
-                  {t("save")}
-                </button>
-                {isSaving && <Loading />}
-              </div>
-              {currentAddress?.id > 0 && (
-                <div className="sm:col-span-6">
-                  <button
-                    htmlFor={2}
-                    type="button"
-                    className="block cursor-pointer select-none  bg-red-500 p-2 text-center  text-white w-full"
-                    onClick={deleteAddress}
-                  >
-                    {t("delete")}
-                  </button>
-                </div>
-              )}
             </>
           )}
         </div>
       </form>
+
       {false && (
         <div className="w-full pt-10">
           <h2 className="text-2xl  text-gray-900 uppercase font-normal">
